9f392343b0e613ffab61ab99b77080bb1e0be61b
Crii-CL <crisrecon14@gmail.com>
2023-01-08 19:40:57 -0500

    Stage 4 part 3 Code getCardElement

--- scripts/index.js
+++ scripts/index.js
@@ -28,11 +28,32 @@ const initialCards = [
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileEditClose = document.querySelector("#profile-edit-close");
+const profileTitle = document.querySelector("#profile-title");
+const profileSubtitle = document.querySelector("#profile-subtitle");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileSubtitleInput = document.querySelector("#profile-subtitle-input");
+const modalSaveBtn = document.querySelector(".modal__form_button");
+const cardTemplate = document.querySelector("#card-template");
 
 profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileSubtitleInput.value = profileSubtitle.textContent;
   profileEditModal.classList.add("modal_opened");
 });
 
 profileEditClose.addEventListener("click", () => {
   profileEditModal.classList.remove("modal_opened");
 });
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileSubtitle.textContent = profileSubtitleInput.value;
+  profileEditModal.classList.remove("modal_opened");
+}
+
+modalSaveBtn.addEventListener("click", handleProfileFormSubmit);
+
+// getCardElement(){
+
+// }
