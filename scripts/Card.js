fee5b5d27069b584988b3df2bcf97605c44a2ddc
Crii-CL <crisrecon14@gmail.com>
2023-02-17 23:37:42 -0500

    setting event listeners

--- scripts/Card.js
+++ scripts/Card.js
@@ -10,10 +10,42 @@ class Card {
     return document
       .querySelector(this._cardSelector)
       .content.querySelector(".card")
-      .cloneNOde(true);
+      .cloneNode(true);
   }
 
-  getView() {}
+  _setEventListeners() {
+    this._cardDelBtn
+      .querySelector(".card__delete-button")
+      .addEventListener("click", this._handleDelButton);
+    this._cardLikeBtn
+      .querySelector(".card__like-button")
+      .addEventListener("click", this._handleLikeButton);
+    this._cardImageEl
+      .querySelector(".card__image")
+      .addEventListener("click", () => {
+        handleImagePreview(cardData);
+        openPopup(modalPreview);
+      });
+  }
+
+  _handleDelBtn(e) {
+    const card = e.target.closest("#card");
+    card.remove();
+  }
+
+  _handleLikeBtn(e) {
+    e.target.classList.toggle("card__like-button_active");
+  }
+
+  getView() {
+    const template = this._getTemplate;
+    this._setEventListeners();
+  }
 }
 
 export default Card;
+
+// It takes card data — text and a link to the image — and a template element selector as parameters into the constructor.
+// It has private methods for working with markup and adding event listeners.
+// It has private methods for each event handler.
+// It has one public method that returns a fully functional card element populated with data.
