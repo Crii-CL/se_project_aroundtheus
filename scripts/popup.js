5718024445e019d5a4817ab828df267e863ce739
Crii-CL <crisrecon14@gmail.com>
2023-03-03 21:23:35 -0500

    refactoring popup

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,36 +1,35 @@
 export default class Popup {
-  contructor({ popupSelector }) {
+  constructor({ popupSelector }) {
     this._popupElement = document.querySelector(popupSelector);
   }
 
   open() {
     this._popupElement.classList.add("modal_opened");
+    document.addEventListener("keydown", () => this._handleEscClose());
+    document.addEventListener("mousedown", () => this._handleOverlay());
   }
 
   close() {
     this._popupElement.classList.remove("modal_opened");
+    document.removeEventListener("keydown", () => this._handleEscClose());
+    document.removeEventListener("mousedown", () => this._handleOverlay());
   }
 
   _handleEscClose(e) {
     if (e.key === "Escape") {
       // const openedPopup = document.querySelector(".modal_opened");
-      close(this._popupElement);
+      this.close();
     }
   }
 
-  _setEventListeners() {
-    if (this._popupElement.open()) {
-      this._popupElement.addEventListener("keydown", closeByEscape);
-      this._popupElement.addEventListener("mousedown", handleOverlay);
-    } else if (this._popupElement.close()) {
-      this._popupElement.removeEventListener("keydown", closeByEscape);
-      this._popupElement.removeEventListener("mousedown", handleOverlay);
+  _handleOverlay(e) {
+    if (e.target !== this._popupElement) {
+      this.close();
     }
   }
 }
 
-const newPopup = new Popup("modal");
+const newPopup = new Popup({ popupSelector: ".modal" });
 newPopup.open();
 newPopup.close();
 newPopup._handleEscClose();
-newPopup._setEventListeners();
