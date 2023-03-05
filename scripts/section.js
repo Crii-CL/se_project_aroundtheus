0285e8eb12726e34ea2c1f528c090799087030f2
Crii-CL <crisrecon14@gmail.com>
2023-03-05 01:39:18 -0500

    added notes for all the classes

--- scripts/section.js
+++ scripts/section.js
@@ -2,10 +2,17 @@ export default class Section {
   constructor({ items, renderer }, cardEl) {
     this._items = items;
     this._renderer = renderer;
-    this._cardEl = document.querySelector("#card");
+    this._cardEl = document.querySelector("#cards-list");
+    //items serves as an array of data which you need to add on a page when initializing a class
+    //renderer is a function responsible for createing and rendering data
   }
 
-  renderItems() {}
+  renderItems() {
+    //renders all elements on the page
+    //renderer will render them
+  }
 
-  addItem() {}
+  addItem() {
+    //takes DOM element and adds it to the container
+  }
 }
---
+++ scripts/userInfo.js
@@ -0,0 +1,11 @@
+//Take an object with the selectors of two elements into the constructor: one
+//containing the user's name, and another containing the user's job.
+
+//Store a public method named getUserInfo(), which returns an object with
+//information about the user. This method will be handy for cases when it's
+//necessary to display the user data in the open form.
+
+//Store a public method named setUserInfo(), which takes new user data and adds it on the page.
+
+//Create an instance of the UserInfo class in index.js. Use its method setUserInfo()
+//to handle the form submission inside an instance of the PopupWithForm class.
